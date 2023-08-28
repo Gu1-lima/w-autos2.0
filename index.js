2819269b17be5835d5756c36477af47640de54b9
Guilherme Eduardo Lima Barros <00001082384021sp@aluno.educacao.sp.gov.br>
2023-08-28 20:43:38 -0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let list = document.querySelector(".btn-menu");
+let infos = document.querySelector(".infos-item");
 
 list.addEventListener("click", ()=>{
     list.classList.toggle("active");
+    infos.classList.toggle("active");
 })
