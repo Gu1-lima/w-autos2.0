bf6dbec7b7d26fe44f47e3a38c3f2c42d56394ae
Gu1-lima <guilherme.eduliam@gmail.com>
2023-09-15 21:10:22 -0300

    Dialog/modal e adição de novas imagens

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let list = document.querySelector(".btn-menu");
 let infos = document.querySelector(".infos-item");
+let btnModal = document.querySelector('.car-btn');
+let carModal = document.querySelector('dialog')
+
+btnModal.onclick = function (){
+    carModal.showModal();
+}
 
 list.addEventListener("click", ()=>{
     list.classList.toggle("active");
