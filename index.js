2a84429b746e0c932359f9de29b1751971e6a772
Gu1-lima <guilherme.eduliam@gmail.com>
2023-09-18 20:45:04 -0300

    finalizando o dialog e adição de novas imgems

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 let list = document.querySelector(".btn-menu");
 let infos = document.querySelector(".infos-item");
 let btnModal = document.querySelector('.car-btn');
-let carModal = document.querySelector('dialog')
+let carModal = document.querySelector('dialog');
+let btnClose = document.querySelector('.btn-close');
 
-btnModal.onclick = function (){
+btnModal.onclick = function () {
     carModal.showModal();
 }
 
-list.addEventListener("click", ()=>{
+btnClose.onclick = function () {
+    carModal.close();
+}
+
+let blue = document.querySelector('.color-blue');
+    blue.addEventListener("click", () => {
+    blue.classList.toggle("Clicked");
+})
+
+let black = document.querySelector('.color-black');
+    black.addEventListener("click", () => {
+    black.classList.toggle("Clicked");
+})
+
+let dish = document.querySelector('.color-dish');
+    dish.addEventListener("click", () => {
+    dish.classList.toggle("Clicked");
+})
+
+let white = document.querySelector('.color-white');
+    white.addEventListener("click", () => {
+    white.classList.toggle("Clicked");
+})
+
+list.addEventListener("click", () => {
     list.classList.toggle("active");
     infos.classList.toggle("active");
-})
+});
+
