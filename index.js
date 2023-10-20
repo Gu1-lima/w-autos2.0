94edc9d096874a2127cf62a10539672a6d4b355d
Gu1-lima <guilherme.eduliam@gmail.com>
2023-10-20 20:41:41 -0300

    finalizando a  logica ao javascript

--- index.js
+++ index.js
@@ -82,6 +82,54 @@ btnModelos.forEach(elemento => {
                 let priceEvoqueDefault = document.querySelector(".car-evoque");
                 priceEvoqueDefault.innerHTML = `R$ 459.950 <br>Ou até 60x`
             break;
+            case "GP":
+                let priceBwm320i = document.querySelector(".car-bmw320i");
+                priceBwm320i.innerHTML = `R$ 315.950 <br>Ou até 60x`
+            break;
+            case "Sport":
+                let priceBwm320Default = document.querySelector(".car-bmw320i");
+                priceBwm320Default.innerHTML = `R$ 335.950 <br>Ou até 60x`
+            break;
+            case "Jeep-Sport":
+                let priceJeepSportDefault = document.querySelector(".car-jeep-compass");
+                priceJeepSportDefault.innerHTML = `R$ 178.590 <br>Ou até 60x`
+            break;
+            case "Jeep-Longitude":
+                let priceJeepLongitudeDefault = document.querySelector(".car-jeep-compass");
+                priceJeepLongitudeDefault.innerHTML = `R$ 192.390 <br>Ou até 60x`
+            break;
+            case "Jeep-Limited":
+                let priceJeepLimitedDefault = document.querySelector(".car-jeep-compass");
+                priceJeepLimitedDefault.innerHTML = `R$ 215.890 <br>Ou até 60x`
+            break;
+            case "Jeep-SérieS":
+                let priceJeepSérieSDefault = document.querySelector(".car-jeep-compass");
+                priceJeepSérieSDefault.innerHTML = `R$ 230.990 <br>Ou até 60x`
+            break;
+            case "Jeep-Trailhawk":
+                let priceJeepTrailhawkDefault = document.querySelector(".car-jeep-compass");
+                priceJeepTrailhawkDefault.innerHTML = `R$ 260.490 <br>Ou até 60x`
+            break;
+            case "Jmercedes-GLB-progressive":
+                let pricemercedesGLBProgressiveDefault = document.querySelector(".car-mercedes-GLB");
+                pricemercedesGLBProgressiveDefault.innerHTML = `R$ 386.900 <br>Ou até 60x`
+            break;
+            case "mercedes-GLB-AMG":
+                let pricemercedesGLBAMGDefault = document.querySelector(".car-mercedes-GLB");
+                pricemercedesGLBAMGDefault.innerHTML = `R$ 532.900 <br>Ou até 60x`
+            break;
+            case "BMW-X1-GP":
+                let priceBMWX1GPDefault = document.querySelector(".car-BMW-X1");
+                priceBMWX1GPDefault.innerHTML = `R$ 299.950 <br>Ou até 60x`
+            break;
+            case "BMW-X1-X-line":
+                let priceBMWX1XLineDefault = document.querySelector(".car-BMW-X1");
+                priceBMWX1XLineDefault.innerHTML = `R$ 330.950 <br>Ou até 60x`
+            break;
+            case "BMW-X1-M-Sport":
+                let priceBMWX1XMSportDefault = document.querySelector(".car-BMW-X1");
+                priceBMWX1XMSportDefault.innerHTML = `R$ 350.950 <br>Ou até 60x`
+            break;
        }
        //changePrice();
    })
